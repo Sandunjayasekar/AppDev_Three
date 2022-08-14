ee4928145f0e073c7e776879e78d5fe010ba353e
Sandun Jayasekara <jmsrjayasekara97@gmail.com>
2022-08-14 16:04:27 +0530

    Intial App 3

---
+++ app/src/main/java/com/sandun97/AppDevTwo/FirstFragment.kt
@@ -0,0 +1,72 @@
+package com.sandun97.AppDevTwo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.FirebaseFirestore
+import com.sandun97.AppDevTwo.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    private var userEmail: String = ""
+    private var userFirstName: String = ""
+    private var userLastName: String = ""
+    private var userAge: String = ""
+
+    var db = FirebaseFirestore.getInstance()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.submitButton.setOnClickListener {
+            binding.apply {
+
+                // Get the input from the user
+                userEmail = email.text.toString()
+                userFirstName = firstName.text.toString()
+                userLastName = lastName.text.toString()
+                userAge = age.text.toString()
+
+                if (userEmail.isEmpty() || userFirstName.isEmpty() || userLastName.isEmpty() || userAge.isEmpty()) {
+                    // If the user did not enter anything, show an error message
+                    errorView.setText("Please fill all details")
+                } else {
+
+                    // save the user details in the database
+                    db.collection("users").document(userEmail).set(mapOf(
+                        "email" to userEmail,
+                        "firstName" to userFirstName,
+                        "lastName" to userLastName,
+                        "age" to userAge
+                    ))
+                }
+            }
+        }
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/sandun97/AppDevTwo/SecondFragment.kt
@@ -0,0 +1,61 @@
+package com.sandun97.AppDevTwo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.FirebaseFirestore
+import com.sandun97.AppDevTwo.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    var db = FirebaseFirestore.getInstance()
+
+    private var userEmail: String = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.getButton.setOnClickListener {
+            binding.apply {
+
+                // Get the input from the user
+                userEmail = email.text.toString()
+
+                // get the user's data from the database
+                db.collection("users").document(userEmail).get().addOnSuccessListener { document ->
+                    if (document != null) {
+                        // set the data to the textviews
+                        firstNameEdit.text = document.data?.get("firstName").toString()
+                        lastNameEdit.text = document.data?.get("lastName").toString()
+                        ageEdit.text = document.data?.get("age").toString()
+                    }
+                }
+            }
+        }
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
